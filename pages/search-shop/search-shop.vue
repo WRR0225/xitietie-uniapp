99d8ec190aacc6260b22b553459da100e9e558c5
WRR <10811121+wrr0225@user.noreply.gitee.com>
2024-06-12 23:13:31 +0800

    删git重新上传，当前进度：门店列表，城市索引带参跳转门店详情，搜索门店初步

---
+++ pages/search-shop/search-shop.vue
@@ -0,0 +1,16 @@
+<template>
+	<view>
+		<uni-search-bar @confirm="search" @input="input" @cancel="cancelSearch" placeholder='请输入门店或地址关键字' :radius="20" clearButton="always" :focus="true"></uni-search-bar>
+	</view>
+</template>
+
+
+<script setup lang="ts">
+	const cancelSearch =()=>{
+		uni.navigateBack()
+	}
+</script>
+
+<style>
+
+</style>
