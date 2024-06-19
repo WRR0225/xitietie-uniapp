952a4e530a568c4683a465ce1b5ba98f46efd893
WRR <10811121+wrr0225@user.noreply.gitee.com>
2024-06-19 22:27:11 +0800

    初步使用官方api获取头像和昵称

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="my">my
-  </div>
+	<view class="container">
+		<button class="avatar-wrapper" open-type="chooseAvatar" @chooseavatar="onChooseAvatar">
+			<image class="avatar" :src=avatarUrl></image>
+		</button>
+		<input type="nickname" class="weui-input" placeholder="请输入昵称" />
+	</view>
 </template>
 
 <script setup>
+	import {
+		ref
+	} from 'vue'
 
+	const defaultAvatarUrl =
+		'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+	const avatarUrl = ref(defaultAvatarUrl)
+
+	function onChooseAvatar(e) {
+		const {
+			avatarUrl: newAvatarUrl
+		} = e.detail
+		avatarUrl.value = newAvatarUrl
+	}
 </script>
 
-<style scoped></style>
+<style></style>
