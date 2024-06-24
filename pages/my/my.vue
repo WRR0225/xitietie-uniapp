d43ef0b98719f797a624082a5a8dfeac2e52682a
WRR <10811121+wrr0225@user.noreply.gitee.com>
2024-06-24 23:31:45 +0800

    “我的”页-头像、昵称布局调整

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,9 +1,13 @@
 <template>
-	<view class="container">
-		<button class="avatar-wrapper" open-type="chooseAvatar" @chooseavatar="onChooseAvatar">
-			<image class="avatar" :src=avatarUrl></image>
-		</button>
-		<input type="nickname" class="weui-input" placeholder="请输入昵称" />
+	<view class="top">
+		<view class="avatar-box">
+			<button class="avatar-wrapper" open-type="chooseAvatar" @chooseavatar="onChooseAvatar">
+				<image class="avatar" :src=avatarUrl></image>
+			</button>
+		</view>
+		<view class="nickname">
+			<input type="nickname" class="weui-input" placeholder="请输入昵称" />
+		</view>
 	</view>
 </template>
 
@@ -24,4 +28,37 @@
 	}
 </script>
 
-<style></style>
+<style lang='scss'>
+	.top {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		.avatar-box {
+			margin-top: 20px;
+			.avatar-wrapper {
+				
+				width: 100px;
+				height: 100px;
+				padding: 0; // 移除按钮的内边距
+				border: none; // 移除按钮的边框
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				border-radius: 50%; // 将按钮本身设置为圆形
+
+				.avatar {
+					width: 100%;
+					height: 100%;
+					display: block; // 设置图像为块级元素
+					border-radius: 50%; // 将图像设置为圆形
+				}
+			}
+		}
+		.nickname{
+			margin-top: 20px;
+			width: 200px;
+			text-align: center;
+		}
+	}
+</style>
